0be793e7b37f32ded62d7cea0117d5ea51a97d94
heisenferg <heisenferg@gmail.com>
2021-12-22 20:58:35 +0100

    2

--- app/src/main/java/com/example/birthdayreminder/MainActivity.java
+++ app/src/main/java/com/example/birthdayreminder/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.birthdayreminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listaContactos;
+    SQLiteDatabase db;
+    EditText contactos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contactos = findViewById(R.id.eTNombres);
+        listaContactos = findViewById(R.id.listViewLista);
+
+        // Base de datos.
+
+        db = openOrCreateDatabase("Cumpleaños", Context.MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS miscumples(\n" +
+                " ID integer,\n" +
+                " TipoNotif char(1),\n" +
+                " Mensaje VARCHAR(160),\n" +
+                " Telefono VARCHAR(15),\n" +
+                " FechaNacimiento VARCHAR(15),\n" +
+                " Nombre VARCHAR(128)\n" +
+                ");");
+
     }
 }
