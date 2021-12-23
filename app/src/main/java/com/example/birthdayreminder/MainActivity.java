291d10500fd1666c63286f24416d1f9cdd995068
heisenferg <heisenferg@gmail.com>
2021-12-23 13:42:10 +0100

    2

--- app/src/main/java/com/example/birthdayreminder/MainActivity.java
+++ app/src/main/java/com/example/birthdayreminder/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.birthdayreminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     ListView listaContactos;
     SQLiteDatabase db;
     EditText contactos;
@@ -16,9 +26,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        contactos = findViewById(R.id.eTNombres);
-        listaContactos = findViewById(R.id.listViewLista);
+        solicitarPermisos();
+        contactos = findViewById(R.id.etNombres);
 
         // Base de datos.
 
@@ -32,5 +41,52 @@ public class MainActivity extends AppCompatActivity {
                 " Nombre VARCHAR(128)\n" +
                 ");");
 
+        Button buscar = findViewById(R.id.buttonBuscar);
+        buscar.setOnClickListener(this);
+
+
+
+    }
+
+
+    private void solicitarPermisos(){
+        boolean permisos_concedidos=false;
+
+        while (permisos_concedidos==false) {
+            if (checkSelfPermission(Manifest.permission.READ_CONTACTS) !=
+                    PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 1);
+                requestPermissions(new String[]{Manifest.permission.SEND_SMS}, 1);
+            } else if (checkSelfPermission(Manifest.permission.READ_CONTACTS) ==
+                    PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
+                permisos_concedidos=true;
+            }
+        }
+    }
+
+    @SuppressLint("Range")
+    @Override
+    public void onClick(View v) {
+        //Buscamos Contactos
+        String proyeccion[]={ContactsContract.Contacts._ID,
+                ContactsContract.Contacts.DISPLAY_NAME,
+                ContactsContract.Contacts.HAS_PHONE_NUMBER,
+                ContactsContract.CommonDataKinds.Event.START_DATE,
+                ContactsContract.Contacts.PHOTO_ID};
+
+        // FROM
+        Uri contactUri = ContactsContract.Contacts.CONTENT_URI;
+
+        // WHERE ORDER BY
+        ContentResolver cr = getContentResolver();
+        Cursor misContactos = cr.query(contactUri, proyeccion, null, null, null);
+
+        if (misContactos!=null){
+            while(misContactos.moveToNext()){
+                String idContacto = misContactos.getString(misContactos.getColumnIndex(ContactsContract.Contacts._ID));
+                String nombrecontacto = misContactos.getString(misContactos.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                Log.d("Contacto: ", idContacto + ": " + nombrecontacto);
+            }
+        }
     }
 }
